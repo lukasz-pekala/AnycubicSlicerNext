e84206d7ec35a4679b143c7b8f062d66d2353690
liucl <66@6666.com>
2025-08-12 17:23:59 +0800

    ✨ feat(deps): 增加依赖库编译

---
+++ deps/plugins/plugins.cmake
@@ -0,0 +1,7 @@
+﻿orcaslicer_add_cmake_project(
+    OpenPlugins
+    GIT_REPOSITORY "http://10.0.169.101/qprj/fdm/open-plugins.git"
+    GIT_TAG "main"
+    GIT_SHALLOW ON
+    DEPENDS ${WXWIDGETS_PKG}
+)
