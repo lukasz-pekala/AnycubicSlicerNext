33425b7a4836018b3cf96fb86ac500aa17035252
liucl <66@6666.com>
2025-08-28 17:21:39 +0800

    🌈 style: 减少编译警告

--- src/libslic3r/ExtrusionEntity.hpp
+++ src/libslic3r/ExtrusionEntity.hpp
@@ -610,7 +610,7 @@ inline void extrusion_entities_append_paths_with_wipe(ExtrusionEntitiesPtr &dst,
                 Point temp = polyline.first_point() - last_end_point;
                 if (Vec2d(temp.x(), temp.y()).norm() <= 3 * scaled(width)) {
                     multi_path->paths.push_back(ExtrusionPath(role, mm3_per_mm, width, height, true));
-                    multi_path->paths.back().polyline = std::move(Polyline(last_end_point, polyline.first_point()));
+                    multi_path->paths.back().polyline = Polyline(last_end_point, polyline.first_point());
                 } else {
                     dst.push_back(multi_path);
                     multi_path = new ExtrusionMultiPath();
