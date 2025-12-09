6f4a6b00746407ca8cea7c0045fe924629ee9ab8
liucl <6666@66.com>
2025-12-09 10:56:24 +0800

    build: 修正windows下编译问题

--- src/slic3r/Anycubic/plugins/xrc/detail/xrc_pluigns.cpp
+++ src/slic3r/Anycubic/plugins/xrc/detail/xrc_pluigns.cpp
@@ -12,30 +12,30 @@
 
 namespace Slic3r::GUI {
 
-// 自定义控件列表--如果添加自定义控件，需要在这添加到CONTORL_LIST宏中
-// 这里的类型要求有默认构造函数
-#define CONTORL_LIST (AMSrefresh, Button, ErrorMsgStaticText)
+// // 自定义控件列表--如果添加自定义控件，需要在这添加到CONTORL_LIST宏中
+// // 这里的类型要求有默认构造函数
+// #define CONTORL_LIST (ErrorMsgStaticText)
 
-#define HANDLER_TYPE_NAME(type) BOOST_PP_CAT(ControlHandler_, type)
+// #define HANDLER_TYPE_NAME(type) BOOST_PP_CAT(ControlHandler_, type)
 
-#define CONTROL_HANDLER(r, nil, type)                                          \
-  struct HANDLER_TYPE_NAME(type) : public wxXmlResourceHandler {               \
-    wxObject *DoCreateResource() override { return new type; }                 \
-    bool CanHandle(wxXmlNode *node) override {                                 \
-      return IsOfClass(node, wxT(BOOST_PP_STRINGIZE(type)));                   \
-    }                                                                          \
-    DECLARE_DYNAMIC_CLASS(HANDLER_TYPE_NAME(type));                            \
-  };                                                                           \
-  wxIMPLEMENT_DYNAMIC_CLASS(HANDLER_TYPE_NAME(type), wxXmlResourceHandler);
+// #define CONTROL_HANDLER(r, nil, type)                                          \
+//   struct HANDLER_TYPE_NAME(type) : public wxXmlResourceHandler {               \
+//     wxObject *DoCreateResource() override { return new type; }                 \
+//     bool CanHandle(wxXmlNode *node) override {                                 \
+//       return IsOfClass(node, wxT(BOOST_PP_STRINGIZE(type)));                   \
+//     }                                                                          \
+//     DECLARE_DYNAMIC_CLASS(HANDLER_TYPE_NAME(type));                            \
+//   };                                                                           \
+//   wxIMPLEMENT_DYNAMIC_CLASS(HANDLER_TYPE_NAME(type), wxXmlResourceHandler);
 
-BOOST_PP_SEQ_FOR_EACH(CONTROL_HANDLER, nil, BOOST_PP_TUPLE_TO_SEQ(CONTORL_LIST))
+// BOOST_PP_SEQ_FOR_EACH(CONTROL_HANDLER, nil, BOOST_PP_TUPLE_TO_SEQ(CONTORL_LIST))
 
 #define REGISTER_HANDLER(r, nil, type)                                         \
   wxXmlResource::Get()->AddHandler(new HANDLER_TYPE_NAME(type));
 
 XrcPlugin::XrcPlugin(Anycubic::Plugins::PluginHost *host) {
-  BOOST_PP_SEQ_FOR_EACH(REGISTER_HANDLER, nil,
-                        BOOST_PP_TUPLE_TO_SEQ(CONTORL_LIST));
+  // BOOST_PP_SEQ_FOR_EACH(REGISTER_HANDLER, nil,
+  //                       BOOST_PP_TUPLE_TO_SEQ(CONTORL_LIST));
 }
 XrcPlugin::~XrcPlugin() {}
 } // namespace Slic3r::GUI
