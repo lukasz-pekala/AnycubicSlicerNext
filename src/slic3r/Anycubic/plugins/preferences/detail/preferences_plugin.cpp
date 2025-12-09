82eacde310cd0297da7b3f68d77e57c4ffb7e6ff
liucl <66@6666.com>
2025-12-09 17:58:08 +0800

    feat: 支持配置处理

---
+++ src/slic3r/Anycubic/plugins/preferences/detail/preferences_plugin.cpp
@@ -0,0 +1,50 @@
+#include "preferences_plugin.hpp"
+
+#include <plugins_base/funcation.hxx>
+
+#include <slic3r/GUI/Preferences.hpp>
+
+PreferencesPlugin::PreferencesPlugin(Anycubic::Plugins::PluginHost *host)
+    : host_(host) {
+  auto router = host_->Router();
+  assert(router != nullptr);
+
+  router->REGISTER_FUNCATION(PreferencesPlugin, register_tab);
+
+  host_->WatchWindow("Preferences", this, [](void *ctx, wxWindow *wnd) {
+    assert(wxIsMainThread());
+    PreferencesPlugin *p = reinterpret_cast<PreferencesPlugin *>(ctx);
+    p->CreateTabs(wnd);
+  });
+}
+
+void PreferencesPlugin::CreateTabs(wxWindow *parent) {
+  assert(parent != nullptr);
+  auto dialog = dynamic_cast<Slic3r::GUI::PreferencesDialog *>(parent);
+  assert(dialog != nullptr);
+  for (auto &tab : tabs_) {
+    auto panel = host_->CreatePanel(dialog, tab.xrcName, tab.xrc);
+    assert(panel != nullptr);
+    auto p = host_->GetPlugin(tab.pluginName.utf8_string().c_str());
+    assert(p != nullptr);
+    p->CreatePanel(panel);
+    // 写入tab到parent
+    auto result = dialog->InsertPanel(tab.title, panel, -1, tab.iconName);
+
+    LOG_INFO("InsertPanel :{}, result={}", tab.title.utf8_string(), result);
+  }
+}
+
+bool PreferencesPlugin::register_tab(const wxString &plugin_name,
+                                     const wxString &title,
+                                     const wxString &icon,
+                                     const wxString &xrcName,
+                                     const wxString *xrc) {
+  assert(xrc != nullptr && xrc->IsEmpty() == false);
+  assert(plugin_name.IsEmpty() == false);
+  if (host_->HasPlugin(plugin_name.utf8_string().c_str())) {
+    return false;
+  }
+  tabs_.push_back({plugin_name, title, icon, xrcName, *xrc});
+  return false;
+}
---
+++ src/slic3r/Anycubic/plugins/preferences/detail/preferences_plugin.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "plugins_manager/plugins_manager.hxx"
+
+#include <boost/preprocessor/cat.hpp>
+
+#include "plugins_base/plugins.hxx"
+#include "plugins_base/plugins_base.hxx"
+
+#include <vector>
+
+#define PLGUINS_NAME preferences
+#define PLUGIN_NAME_STR BOOST_PP_STRINGIZE(PLGUINS_NAME)
+
+class PreferencesPlugin : public Anycubic::Plugins::Plugin {
+public:
+  PreferencesPlugin(Anycubic::Plugins::PluginHost *host);
+  virtual ~PreferencesPlugin() {}
+
+public:
+  void CreateTabs(wxWindow *parent);
+
+private:
+  bool register_tab(const wxString &plugin_name, const wxString &title,
+                    const wxString &icon, const wxString &xrcName,
+                    const wxString *xrc);
+
+private:
+  // Anycubic::Plugins::Plugin interface
+  const char *Name(void) override { return PLUGIN_NAME_STR; }
+  bool Start(void) override { return true; }
+  bool AttachEvt(wxEvtHandler *evt) override { return false; }
+  bool DetachEvt(wxEvtHandler *evt) override { return false; }
+  bool CreatePanel(class wxPanel *panel, wxWindow *parent = nullptr,
+                   wxString *bmp = nullptr) override {
+    return false;
+  }
+  bool CreateWebview(wxWebView *view, wxWindow *parent = nullptr,
+                     wxString *bmp = nullptr) override {
+    return false;
+  }
+  void Destroy(void) override { delete this; }
+
+private:
+  Anycubic::Plugins::PluginHost *host_;
+  struct Tab {
+    wxString pluginName;
+    wxString title;
+    wxString iconName;
+    wxString xrcName;
+    wxString xrc;
+  };
+  std::vector<Tab> tabs_;
+};
