3d038e65e542e54e8189ba9361662e263a32d84f
liucl <6666@66.com>
2026-01-21 14:28:44 +0800

    docs: 更新仓库地址

--- deps/plugins/plugins.cmake
+++ deps/plugins/plugins.cmake
@@ -1,6 +1,6 @@
 ﻿AnycubicSlicer_add_cmake_project(
     OpenPlugins
-    GIT_REPOSITORY "http://10.0.169.101/qprj/fdm/open-plugins.git"
+    GIT_REPOSITORY "https://github.com/ANYCUBIC-3D/open-plugins.git"
     GIT_TAG "develop"
     GIT_SHALLOW ON
     DEPENDS ${WXWIDGETS_PKG} ${OPENSSL_PKG} dep_Boost ${CURL_PKG}
