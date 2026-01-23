8e02d463bb17d27797596370ef4b980229efcc9e
wangliang <wangliang@anycubic.com>
2026-01-23 14:05:30 +0800

    修改部分内容

---
+++ src/slic3r/Anycubic/plugins/xrc/detail/widgets/RemotePrintingAmsDialog.hpp
@@ -0,0 +1,111 @@
+#pragma once
+
+#include <wx/wx.h>
+
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include "PopupWindow.hpp"
+#include "Button.hpp"
+#include "custom_struct_manger.hxx"
+#include <wx/dcbuffer.h>
+#include "../RemotePrint_plugin.hpp"
+
+class ColorBoxObject : public wxNavigationEnabled<StaticBox>
+{
+public:
+    ColorBoxObject(Anycubic::Plugins::RemotePrintPlugin* plugin,wxWindow* parent,
+                   wxSize    winSize,
+                   AmsSlotObjInfo amsSlotBoj,
+                   bool      isCheck   = false,
+                   std::string deviceID = "");
+    ~ColorBoxObject() = default;
+
+    void Init();
+    void SetCheckState(bool index);
+
+    int GetSlotNum() { return m_showSlotNum; }
+    void     mouseDown(wxMouseEvent& event);
+    void     mouseLeave(wxMouseEvent& event);
+    void     mouseEnter(wxMouseEvent& event);
+    void     render(wxAutoBufferedPaintDC& dc);
+    void     edgeColorRender(wxAutoBufferedPaintDC&    dc,
+                             const wxPoint&            point,
+                             const wxColour& color, int type  = 0);
+    void     paintEvent(wxPaintEvent& evt);
+
+    DECLARE_EVENT_TABLE()
+
+private:
+    wxWindow* m_parent;
+    int       m_showSlotNum{-1};
+    wxBitmap  m_check_ico;
+    wxString  m_showFilamentType;
+    wxColour  m_showColor;
+    wxColour  m_showColor_Text;
+    bool      m_isEnable{true};
+    std::string m_deviceID;
+    wxSize    m_winSize;
+    bool      m_isHover{false};
+    bool      m_isCheck{false};
+    AmsSlotObjInfo m_amsSlotObj;
+    Anycubic::Plugins::RemotePrintPlugin* m_plugin{ nullptr };
+};
+
+
+class ColorAMSBoxDialog : public PopupWindow
+{
+public:
+    ColorAMSBoxDialog(Anycubic::Plugins::RemotePrintPlugin* plugin,wxWindow* parent,
+                      wxWindow* amsParent,
+                      wxString& filamentType,
+                      int       parentIndex,
+                      int         slotNum,
+                      wxPoint&    posPint,
+                      std::string deviceID = "");
+    ColorAMSBoxDialog(ColorAMSBoxDialog&&)          = delete;
+    ColorAMSBoxDialog(const ColorAMSBoxDialog&) = delete;
+    ColorAMSBoxDialog& operator=(ColorAMSBoxDialog&&) = delete;
+    ColorAMSBoxDialog& operator=(const ColorAMSBoxDialog&) = delete;
+    ~ColorAMSBoxDialog()                                       = default;
+
+    void      Init();
+    wxSize    GetWindowSize();
+    wxWindow* GetAmsParentWin() { return m_amsParent; }
+    void      msw_rescale();
+    void      SetShowNewSlotInfo(wxWindow* amsParent, wxString& filamentType, int parentIndex, int slotNum, std::string deviceID);
+    void      SetFilamentType(wxString& filamentType) { m_selectFilamentType = filamentType; }
+    void      SetDeviceID(std::string deviceID) { m_deviceID = deviceID; }
+    int       GetParentIndex() { return m_parentIndex; }
+    wxString  GetFilamentType() { return m_selectFilamentType; }
+    bool      GetMouseIsWin() { return m_isInlcudeWin; }
+
+    void mouseDown(wxMouseEvent& event);
+    void mouseCaptureLost(wxMouseCaptureLostEvent& event);
+    void mouseMove(wxMouseEvent& event);
+    void mouseReleased(wxMouseEvent& event);
+
+    void SendEventToChild(const wxPoint& point,int eventType);
+
+    virtual void OnDismiss() wxOVERRIDE;
+    bool         HasDismissLongTime();
+private:
+    wxWindow *m_parent;
+    wxWindow*                    m_amsParent{nullptr};
+    wxPoint   m_posPint;
+    wxString  m_selectFilamentType;
+    std::string            m_deviceID;
+    int       m_slotSum{-1};
+    int       m_slotNum{-1};
+    int       m_parentIndex{-1};
+    Button*   m_warningInfo{nullptr};
+    std::vector<ColorBoxObject*> m_colorBoxList;
+    ColorBoxObject*              m_lastHoverBox = nullptr;
+    bool                         m_isInlcudeWin{false};
+    bool                         m_pressedDown{false};
+    boost::posix_time::ptime     dismissTime;
+    Anycubic::Plugins::RemotePrintPlugin* m_plugin{ nullptr };
+    DECLARE_EVENT_TABLE()
+};
+
+
+
+
